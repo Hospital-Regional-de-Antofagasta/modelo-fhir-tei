8e7a6849a30ab2fdbfe2b26eefe004c69a49ec43
Christian Farias <christian.farias.aguila@gmail.com>
2025-05-07 15:07:14 -0400

    Cambia el tipo de via de domicilio a opcional - Cambia el numero de calle a opcional

--- sql/tables/extension/DomicilioPaciente.sql
+++ sql/tables/extension/DomicilioPaciente.sql
@@ -5,34 +5,28 @@ IF OBJECT_ID(N'dbo.DomicilioPaciente', N'U') IS NULL BEGIN
 
     NumeroPaciente FLOAT NOT NULL CONSTRAINT FK_DOMICILIO_PACIENTE FOREIGN KEY REFERENCES [dbo].[PAC_Paciente](PAC_PAC_Numero),
 
-    -- FHIR_Line VARCHAR(200) NOT NULL,
-
+    -- Requerido para tributar a FHIR.
     IdUso TINYINT NOT NULL CONSTRAINT FK_DOMICILIO_USO FOREIGN KEY REFERENCES [dbo].[TAB_FHIR_UsoDomicilioPersona](Id),
-
-    -- Capitulo 4 inciso 1.1. Región
-    -- IdRegion CHAR(2) NULL CONSTRAINT FK_DOMICILIO_REGION FOREIGN KEY REFERENCES [dbo].[TAB_DEIS_Region](Id),
-    
-    -- -- Capitulo 4 inciso 1.2. Provincia
-    -- IdProvincia CHAR(3) NULL CONSTRAINT FK_DOMICILIO_PROVINCIA FOREIGN KEY REFERENCES [dbo].[TAB_DEIS_Provincia](Id),
     
     -- Capitulo 4 inciso 1.3. Comuna
     IdComuna CHAR(5) NOT NULL CONSTRAINT FK_DOMICILIO_COMUNA FOREIGN KEY REFERENCES [dbo].[TAB_DEIS_Comuna](Id),
 
     -- Capitulo 4 inciso 1.5.1. Tipo de vía
-    IdTipoVia TINYINT NOT NULL CONSTRAINT FK_DOMICILIO_TIPOVIA FOREIGN KEY REFERENCES [dbo].[TAB_DEIS_TipoVia](Id),
+    IdTipoVia TINYINT NULL CONSTRAINT FK_DOMICILIO_TIPOVIA FOREIGN KEY REFERENCES [dbo].[TAB_DEIS_TipoVia](Id),
 
     -- Capitulo 4 inciso 1.5.2. Nombre de vía
     NombreCalle VARCHAR(100) NOT NULL,
 
     -- Capitulo 4 inciso 1.5.3. Numero
-    NumeroCalle VARCHAR(10) NOT NULL,
+    NumeroCalle VARCHAR(10) NULL,
 
     -- Capitulo 4 inciso 1.5.4. Anexo de dirección
     AnexoCalle VARCHAR(200) NULL,
 
-    -- Capitulo 4 inciso 1.7. Sistema de coordenadas
+    -- Capitulo 4 inciso 1.7. Sistema de coordenadas (obligatorio, pero de momento es imposible implementarlo)
     Latitud VARCHAR(20) NULL,
     Longitud VARCHAR(20) NULL,
+
     CodigoPostal VARCHAR(10) NULL,
 
     [CreatedBy] [VARCHAR](10) NOT NULL CONSTRAINT FK_DOMICILIO_CREATEDBY FOREIGN KEY REFERENCES [dbo].[Segu_Usuarios]([Segu_Usr_Cuenta]),
